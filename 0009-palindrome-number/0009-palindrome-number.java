61b9feae8f66203a22754c079c0613cb53e3f8d0
vignesh8reddy <144072079+vignesh8reddy@users.noreply.github.com>
2023-09-07 20:42:59 -0500

    Time: 5 ms (80.27%), Space: 43.5 MB (9.61%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public boolean isPalindrome(int x) {
         String st = Integer.toString(x);//String st = String.valueOf(x);
@@ -15,3 +16,25 @@ class Solution {
 
 //Time Complexity : Theta(1)
 //Space Complexity : Theta(1)
+
+*/
+
+//More optimised Approach
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0 || (x>0 && x%10==0))
+            return false;
+        int rev=0;
+        int n = x;
+        int temp;
+        while(n>0){
+            temp = n%10;
+            rev=rev*10+temp;
+            n = n/10;
+        }
+        if(rev==x)
+            return true;
+        return false;
+    }
+}
