6a65f908c78cf60b3e9f93fdd17dc237eda31ba8
Vignesh Reddy Ireddy <144072079+vignesh8ireddy@users.noreply.github.com>
2025-02-04 11:59:57 -0600

    Time: 0 ms (100%), Space: 41.3 MB (94.58%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,32 +1,16 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        int n = strs.length;
-        int minStr = 0;
-        int minLen = strs[0].length();
-        String res = "";
-        for(int i=1; i<n; i++) {
-            if(strs[i].length()<minLen) {
-                minStr = i;
-                minLen = strs[i].length();
+        Arrays.sort(strs);
+        String str1 = strs[0];
+        String str2 = strs[strs.length-1];
+        int i = 0;
+        while(i < str1.length() && i < str2.length()){
+            if(str1.charAt(i) == str2.charAt(i)){
+                i++;
+            } else {
+                break;
             }
         }
-        for(int j=0;j<minLen;j++) {
-            boolean flag = true;
-            for(int k=0; k<n; k++) {
-                if(k==minStr) continue;
-                else if(strs[minStr].charAt(j) != strs[k].charAt(j)) {
-                    flag = false;
-                    break;
-                }
-                
-            }
-            if(!flag) break;
-            res += strs[minStr].charAt(j);
-
-        }
-        return res;
+        return str1.substring(0, i);
     }
 }
-
-//Time Complexity: Theta(m*n), m=length of smallest string, n= length of array
-//Space Complexity: Theta(Constant)
